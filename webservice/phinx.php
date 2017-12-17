e1faceb75a08d36713e8807adfbd85928e50cf6f
Roman <iroman.via@gmail.com>
2017-12-17 23:23:20 +0200

    added phinx tool and test migration

---
+++ webservice/phinx.php
@@ -0,0 +1,24 @@
+<?php
+
+(new \Dotenv\Dotenv(__DIR__))->load();
+
+return [
+    'paths' => [
+        'migrations' => __DIR__ . '/app/*/*/db/migrations',
+        'seeds' => __DIR__ . '/app/*/*/db/seeds',
+    ],
+    'environments' => [
+        'default_migration_table' => 'schema_migrations_log',
+        'default_database' => 'development',
+        'development' => [
+            'adapter' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT'),
+            'charset' => getenv('DB_CHARSET'),
+        ],
+    ],
+    'version_order' => 'creation',
+];
