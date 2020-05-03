ddf87095b0b460985333e12ad25ca1c217a6465a
Roman Shevchenko <iroman.via@gmail.com>
2020-05-04 01:13:16 +0300

    [container-service-provider-util] Improved aggregate service provider

---
+++ packages/container-service-provider-util/src/AggregateProvider.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rosem\Component\Container;
+
+use Rosem\Contract\Container\ServiceProviderInterface;
+
+use function get_class;
+
+/**
+ * Trait AbstractAggregateServiceProvider.
+ */
+class AggregateProvider implements ServiceProviderInterface
+{
+    /**
+     * Providers collection.
+     *
+     * @var ServiceProviderInterface[]
+     */
+    protected array $providers;
+
+    /**
+     * Add the service provider to the collection.
+     *
+     * @param ServiceProviderInterface $serviceProvider
+     *
+     * @return void
+     */
+    public function addProvider(ServiceProviderInterface $serviceProvider): void
+    {
+        // todo same validation as in ServiceContainer
+        $this->providers[] = $serviceProvider;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFactories(): array
+    {
+        $factories = [];
+
+        foreach ($this->providers as $provider) {
+            $factories = [get_class($provider) => fn() => $provider] +
+                $provider->getFactories() +
+                $factories;
+        }
+
+        return $factories;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExtensions(): array
+    {
+        $extensions = [];
+
+        foreach ($this->providers as $provider) {
+            /** @noinspection AdditionOperationOnArraysInspection */
+            $extensions = $provider->getExtensions() + $extensions;
+        }
+
+        return $extensions;
+    }
+}
--- packages/http-server/src/Provider/KernelServiceProvider.php
+++ packages/http-server/src/Provider/KernelServiceProvider.php
@@ -1,25 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Rosem\Component\Http\Server\Provider;
 
-use Rosem\Component\Container\AggregateServiceProviderTrait;
-use Rosem\Contract\Container\ServiceProviderInterface;
+use Rosem\Component\Container\AggregateProvider;
 
-class KernelServiceProvider implements ServiceProviderInterface
+/**
+ * Class KernelServiceProvider.
+ */
+class KernelServiceProvider extends AggregateProvider
 {
-    use AggregateServiceProviderTrait;
-
+    /**
+     * KernelServiceProvider constructor.
+     */
     public function __construct()
     {
-        $this->addServiceProviders(
-            [
-                // Need to create server responses
-                MessageServiceProvider::class,
-                // Need to emit a response to a client
-                EmitterProvider::class,
-                // Need to allow middleware usage
-                MiddlewareProvider::class,
-            ]
-        );
+        // Need to create server responses
+        $this->addProvider(new MessageServiceProvider());
+        // Need to emit a response to a client
+        $this->addProvider(new EmitterProvider());
+        // Need to allow middleware usage
+        $this->addProvider(new MiddlewareProvider());
     }
 }
